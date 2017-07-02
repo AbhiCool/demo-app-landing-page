5cb08d816fa5b1eb37fff853efa1957bad050f68
Abhijit Govekar <abhijitnaikgovekar@gmail.com>
2017-07-03 00:19:52 +0530

    removed javascript preloader with image loaded percentage progress bar and added jpreloader plugin as preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,58 +15,30 @@
 $(window).on('resize', function() {
   alignContainer();
 });
-/*script for preloder percentage bar*/
-(function(){
-  function id(v){ return document.getElementById(v); }
-  function loadbar() {
-    var ovrl = id("overlay"),
-        prog = id("progress"),
-        stat = id("progstat"),
-        img = document.images,
-        c = 0,
-        tot = img.length;
-    if(tot == 0) return doneLoading();
 
-    function imgLoaded(){
-      c += 1;
-      var perc = ((100/tot*c) << 0) +"%";
-      prog.style.width = perc;
-      stat.innerHTML = "Loading "+ perc;
-      if(c===tot) return doneLoading();
-    }
-    function doneLoading(){
-      ovrl.style.opacity = 0;
-      alignContainer()
-      setTimeout(function(){
-        ovrl.style.display = "none";
-      }, 1200);
-    }
-    for(var i=0; i<tot; i++) {
-      var tImg     = new Image();
-      tImg.onload  = imgLoaded;
-      tImg.onerror = imgLoaded;
-      tImg.src     = img[i].src;
-    }
-  }
-  document.addEventListener('DOMContentLoaded', loadbar, false);
-}());
+
+$(document).ready(function() {
+   $('body').jpreLoader({
+       loaderVPos: '50%',
+       autoClose: true
+   }, function () { //callback function
+    alignContainer();
+   });
+});
 
 function alignContainer() {
   var windowHeight = $(window).outerHeight(true);
   var containerHeight = $('.container').outerHeight(true);
 
   var valueOfMarginTop = (windowHeight - containerHeight)/2;
-  if(valueOfMarginTop <0){
+  if(valueOfMarginTop < 0){
   valueOfMarginTop = 0;
   }
-  $('.container').css({'margin-top':valueOfMarginTop});
+  $('.container').animate({'margin-top':valueOfMarginTop});
 
   /*animation effects on mobile and mobile screen*/
   $('#mobile-animation').addClass('animated fadeInRight');
-  $('#mobile-animation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-
+  $('#mobile-animation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
       $('.mobile-slider-container').show().addClass('animated fadeIn');
   });
 }
-
-
