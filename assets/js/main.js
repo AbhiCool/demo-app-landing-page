2efc1a631190cc90b160749a89acddfec708ca3f
Abhijit Govekar <abhijitnaikgovekar@gmail.com>
2017-07-02 22:37:42 +0530

    added preloader with image loaded percentage progress bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,25 +12,61 @@
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 /*script for centering div to the center of page*/
-$(window).on('load resize', function() {
+$(window).on('resize', function() {
+  alignContainer();
+});
+/*script for preloder percentage bar*/
+(function(){
+  function id(v){ return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        prog = id("progress"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0,
+        tot = img.length;
+    if(tot == 0) return doneLoading();
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) +"%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading "+ perc;
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      ovrl.style.opacity = 0;
+      alignContainer()
+      setTimeout(function(){
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
+
+function alignContainer() {
   var windowHeight = $(window).outerHeight(true);
   var containerHeight = $('.container').outerHeight(true);
 
   var valueOfMarginTop = (windowHeight - containerHeight)/2;
   if(valueOfMarginTop <0){
-	valueOfMarginTop = 0;
+  valueOfMarginTop = 0;
   }
   $('.container').css({'margin-top':valueOfMarginTop});
-  
-  /*added to fadeout loader and remove it*/
-  $('#preloader').fadeOut('slow',function(){
-      $(this).remove();
-  });
+
   /*animation effects on mobile and mobile screen*/
   $('#mobile-animation').addClass('animated fadeInRight');
   $('#mobile-animation').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      
+
       $('.mobile-slider-container').show().addClass('animated fadeIn');
   });
-});
+}
+
 
